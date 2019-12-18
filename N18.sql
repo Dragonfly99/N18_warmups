293905c2cf0721529e092722a747ea7e970721ae
Kerry <kerrspeed@yahoo.com>
2019-12-18 10:12:43 -0500

    Updated assignment. Got Raank to work

--- N18.sql
+++ N18.sql
@@ -1,10 +1,23 @@
-SELECT customers.customer_id, country, SUM(order_details.unit_price), RANK() OVER(partition by customers.customer_id) total
-FROM customers
+SELECT c.customer_id, country, SUM(order_details.unit_price) as total, RANK() OVER(partition by country Order By SUM(order_details.unit_price) desc) ranking
+FROM customers as c
 join orders
 using(customer_id)
 join order_details
 using (order_id)
 join products
 using (product_id)
-order by total desc
-limit 5;
+group by c.customer_id
+limit 15;
+
+
+SELECT c.customer_id, country, SUM(order_details.unit_price) as total, RANK() OVER(partition by country Order By SUM(order_details.unit_price) desc) ranking
+FROM customers as c
+join orders
+using(customer_id)
+join order_details
+using (order_id)
+join products
+using (product_id)
+where ranking in (1,2,3)
+group by c.customer_id
+limit 15;
